0e71e88564247e7617708d33c65c4974b9da4243
Gerry_Pang <watcher@aliyun.com>
2017-02-02 19:21:20 +0800

    简单查询

--- src/com/pgw/test/MybatisHello.java
+++ src/com/pgw/test/MybatisHello.java
@@ -2,19 +2,23 @@ package com.pgw.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import com.pgw.pojo.MidUser;
 
 public class MybatisHello {
 	
 	public static void main(String[] args) {
-		selectOne();
-		insertOne();
-		updateOne();
-		deleteOne();
+		selectByMap();
+		//selectOne();
+		//insertOne();
+		//updateOne();
+		//deleteOne();
 	}
 	
 	public static void deleteOne(){
@@ -78,6 +82,26 @@ public class MybatisHello {
 		}
 	}
 	
+	public static void selectByMap(){
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = null;
+		SqlSession sqlSession = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			sqlSession = sqlSessionFactory.openSession();
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("usercode", "admin");
+			map.put("password", "202cb962ac59075b964b07152d234b70");
+			MidUser miduser = sqlSession.selectOne("findByMap",map);
+			System.out.println(miduser.getUsername());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+	}
+	
 	public static void selectOne(){
 		String resource = "mybatis-config.xml";
 		InputStream inputStream = null;
@@ -94,4 +118,5 @@ public class MybatisHello {
 			sqlSession.close();
 		}
 	}
+	
 }
